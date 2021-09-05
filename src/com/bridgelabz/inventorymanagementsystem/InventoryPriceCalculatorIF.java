dfc7171ec762f7f287a0a7245bb5dd4c59555352
Akash Singh <akashsingh6280@gmail.com>
2021-09-06 03:51:46 +0530

    ADDED: interface to abstract calculateTotal method

---
+++ src/com/bridgelabz/inventorymanagementsystem/InventoryPriceCalculatorIF.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.inventorymanagementsystem;
+
+public interface InventoryPriceCalculatorIF {
+	public double calculateInventoryValue(InventoryFactory rice,InventoryFactory wheat ,InventoryFactory pulse);
+}

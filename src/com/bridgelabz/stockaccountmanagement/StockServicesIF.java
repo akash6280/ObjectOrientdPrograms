433d73ba57d7361b3e64887001d1a5148b0ee5d0
Akash Singh <akashsingh6280@gmail.com>
2021-09-06 05:21:34 +0530

    ADDED: interface to abstract calculate value of each stock method

---
+++ src/com/bridgelabz/stockaccountmanagement/StockServicesIF.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stockaccountmanagement;
+
+import java.util.ArrayList;
+
+public interface StockServicesIF {
+			public void calulateValueOfEachStock(ArrayList<Stock> stocks);
+}
